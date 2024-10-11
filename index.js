e6deb6562b3f7176ca027d7a381e63424db976ca
YeYint009 <blackskym96@gmail.com>
2024-10-11 19:16:23 +0900

    createServer

--- index.js
+++ index.js
@@ -8,7 +8,20 @@ const fs = require("fs");
 
 // console.log("File Written");
 
-fs.readFile("./txt/readThis.txt", "utf-8", (err, data) => {
-  console.log(data);
-});
-console.log("object")
+// fs.readFile("./txt/readThis.txt", "utf-8", (err, data) => {
+//   console.log(data);
+// });
+// console.log("object")
+const http = require("http");
+// const server = http.createServer((req, res) => {
+//   res.end("Hello from Server");
+// });
+
+// server.listen(8000, "127.0.0.1", () => {
+//   console.log("Listening");
+// });
+
+http.createServer(function (req, res) {
+    res.write("Hello From Server 8000");
+    res.end();
+  }).listen(8080);
