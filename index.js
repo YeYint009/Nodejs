e55381c74993fd8126c1ecbd919a32cf1959ac39
YeYint009 <blackskym96@gmail.com>
2024-10-11 18:30:12 +0900

    study of Node

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+// const txtIn = fs.readFileSync("./txt/random.txt", "utf-8");
+// console.log(txtIn);
+
+// const txtOut = `This is added text : ${txtIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", txtOut);
+
+// console.log("File Written");
+
+fs.readFile("./txt/readThis.txt", "utf-8", (err, data) => {
+  console.log(data);
+});
+console.log("object")
